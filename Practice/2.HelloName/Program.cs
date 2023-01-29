b78151df2eb5f61c1bf6ee1160781267d75774b4
Виктор Борков <mazerat_101@mail.ru>
2023-01-29 19:02:26 +0300

    Создал

---
+++ Practice/2.HelloName/Program.cs
@@ -0,0 +1,5 @@
+﻿// Кода на простое приветствие
+Console.WriteLine("Введите своё имя");
+string user = Console.ReadLine()!;
+Console.Write("Вот те здрасьте, это снова вы, ");
+Console.WriteLine(user);
