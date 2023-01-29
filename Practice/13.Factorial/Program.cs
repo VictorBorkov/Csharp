addda7626e01f44938139567316b4f862f8158f4
Виктор Борков <mazerat_101@mail.ru>
2023-01-29 19:05:12 +0300

    Создал

---
+++ Practice/13.Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Посчитать факториал числа
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int F = 1;
+int i = 1;
+
+if (N >= 0)
+{
+    while (i <= N)
+    {
+        F = F * i;
+        i++;   
+    }
+    Console.Write("Факториал будет: ");
+    Console.Write(F);
+}
+else
+{
+    Console.Write("Для этого значения факториал не может быть посчитан");
+}
+
