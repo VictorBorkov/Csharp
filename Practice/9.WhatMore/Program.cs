85960089522ccc20184308b5d75c7a2fd4d87981
Виктор Борков <mazerat_101@mail.ru>
2023-01-29 19:04:22 +0300

    Создал

---
+++ Practice/9.WhatMore/Program.cs
@@ -0,0 +1,8 @@
+﻿// Определить наибольшее из двух чисел
+Console.Write("Укажите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Укажите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (A > B) Console.WriteLine("Первое больше");
+if (A < B) Console.WriteLine("Второе больше");
