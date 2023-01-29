595fcbe63017416835586b5211637ef7809b1276
Виктор Борков <mazerat_101@mail.ru>
2023-01-29 19:04:35 +0300

    Создал

---
+++ Practice/10.SumEven/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Найти сумму всех четных чисел от 1 и до какого-то значения
+
+Console.Write("Задайте значение, до которого будем считать: ");
+int end = int.Parse(Console.ReadLine()!);
+int sum = 0;
+int count = 0;
+
+while (count < end)
+{
+    sum = sum + count;
+    count = count + 2;    
+}
+Console.Write("Сумма получилась: ");
+Console.WriteLine(sum);
