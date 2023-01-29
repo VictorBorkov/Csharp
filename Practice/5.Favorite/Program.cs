63656a85c55e833eb18bfa76d60bfe0ec9f731f1
Виктор Борков <mazerat_101@mail.ru>
2023-01-29 19:03:21 +0300

    Создал

---
+++ Practice/5.Favorite/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача на особое приветсвие, если это любимчик. Это первая задача на ветвление блок-схемы
+Console.Write("Добрый день, введите имя: ");
+string user = Console.ReadLine()!;
+
+if (user.ToLower() == "саша")
+{
+    Console.WriteLine("Йоу, Йоу, Саня, как дела братан?");
+}
+else
+{
+    Console.Write("Здравствуйте, ");
+    Console.WriteLine(user);
+}
