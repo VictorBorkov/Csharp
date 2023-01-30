2bd46731643b605f4d79ae1ca07b6cddd49dfd9d
Виктор Борков <mazerat_101@mail.ru>
2023-01-30 18:47:35 +0300

    Написал код

---
+++ Seminar1-HomeWork/4.MaxOfThree/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Найти максимальное из трех чисел
+
+Console.Write("Укажите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Укажите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write("Укажите третье число: ");
+int C = Convert.ToInt32(Console.ReadLine());
+int max = A;
+
+if (B > max) max = B;
+if (C > max) max = C;
+
+Console.Write("Максимальное значение: ");
+Console.Write(max);
