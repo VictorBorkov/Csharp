4439435c5cf77c7c8fd7a318dac8707975d8e26b
Виктор Борков <mazerat_101@mail.ru>
2023-01-30 18:15:33 +0300

    Создал код

--- Seminar1-HomeWork/2.SquareOrNot/Program.cs
+++ Seminar1-HomeWork/2.SquareOrNot/Program.cs
@@ -1,3 +1,10 @@
 ﻿
 // По двум заданным числам проверять является ли первое квадратом второго
 
+Console.Write("Введите первое значение: ");
+int A = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите второе значение: ");
+int B = Convert.ToInt32(Console.ReadLine()!);
+
+if (B == A * A) Console.WriteLine("Да, является");
+else Console.WriteLine("Нет, не является");
