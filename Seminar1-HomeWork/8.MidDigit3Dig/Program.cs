1c531ceebf9c1b1dbe5e187e6dd27925b41be234
Виктор Борков <mazerat_101@mail.ru>
2023-01-30 22:40:14 +0300

    Написал код

---
+++ Seminar1-HomeWork/8.MidDigit3Dig/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите трёхзначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int second = 0;
+
+if (N > 99 && N < 1000 || N < -99 && N > -1000)
+{
+    if (N > 0) second = N / 10; second = second % 10;
+    if (N < 0) second = (-1)*N / 10; second = second % 10;
+    Console.Write("Вторая цифра: ");
+    Console.Write(second);
+}
+else Console.Write("Вы что, шутите там? ;D");
