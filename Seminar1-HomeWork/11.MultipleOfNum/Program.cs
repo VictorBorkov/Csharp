951446d3f663fc9b1c8f0a3544b8b62d146094e2
Виктор Борков <mazerat_101@mail.ru>
2023-01-31 01:09:11 +0300

    Написал код

---
+++ Seminar1-HomeWork/11.MultipleOfNum/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write("Задайте кратное: ");
+int K = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте делитель: ");
+int D = Convert.ToInt32(Console.ReadLine());
+int ost = K % D;
+
+if (ost == 0)
+{
+    Console.Write("Да, число ");
+    Console.Write(K);
+    Console.Write(" является кратным числа ");
+    Console.WriteLine(D);
+}
+else
+{
+    Console.Write("Нет, не кратное, есть остаток ");
+    Console.Write(ost);
+}
