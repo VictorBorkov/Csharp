b4ab7e783490a87f05bf88c01a5950f0b311c027
Виктор Борков <mazerat_101@mail.ru>
2023-01-30 23:03:43 +0300

    Написал код

---
+++ Seminar1-HomeWork/9.MaxOfSection/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int R = new Random().Next(10, 100);
+Console.Write("Сгенерированное случайное число: ");
+Console.WriteLine(R);
+
+int first = R / 10;
+int second = R % 10;
+int max = 0;
+
+if (first > second) max = first;
+if (second >= first) max = second;
+
+Console.Write("Наибольшая цифра этого числа: ");
+Console.Write(max);
