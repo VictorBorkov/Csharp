28f5255da687e618b6f1b1eed3368c9977e64637
Виктор Борков <mazerat_101@mail.ru>
2023-01-30 18:57:51 +0300

    Написал код

---
+++ Seminar1-HomeWork/5.EvenOrNot/Program.cs
@@ -0,0 +1,8 @@
+﻿
+// Выяснить является ли число чётным
+
+Console.Write("Задайте любое значение: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N % 2 == 0) Console.WriteLine("Да, число четное!");
+else Console.WriteLine("Нет, не четное");
