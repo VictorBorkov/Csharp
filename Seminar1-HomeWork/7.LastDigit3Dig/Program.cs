feef733cc15ddfe9cad81f5020e26d4eb5e406d3
Виктор Борков <mazerat_101@mail.ru>
2023-01-30 22:14:59 +0300

    Написал код

---
+++ Seminar1-HomeWork/7.LastDigit3Dig/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Показать последнюю цифру трёхзначного числа
+
+Console.Write("Введите трёхзначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int three = 0;
+
+if (N > 99 && N < 1000 || N < -99 && N > -1000)
+
+{
+    if (N > 0) three = N % 10;
+    if (N < 0) three = (-1)*N % 10;
+
+    Console.Write("Последняя цифра: ");
+    Console.Write(three);
+}
+
+else Console.Write("Вы что, шутите там? ;D");
+
