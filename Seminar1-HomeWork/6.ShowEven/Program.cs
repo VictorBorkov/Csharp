7da005e370aae26ef4c4b2ad4072049d222569ca
Виктор Борков <mazerat_101@mail.ru>
2023-01-30 19:32:09 +0300

    Написал код

---
+++ Seminar1-HomeWork/6.ShowEven/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// Показать четные числа от 1 до N
+
+Console.Write("Задайте значение, до которого будем показывать четные числа: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+if (N > 1)
+{
+    Console.Write("Чётные числа: ");
+    
+    while (count <= N)
+    {
+        Console.Write(count);
+        Console.Write(", ");
+        count +=2;
+    }
+    Console.Write("вот и всё!");
+}
+else Console.Write("Такое значение не допустимо по условию задачи!");
+
