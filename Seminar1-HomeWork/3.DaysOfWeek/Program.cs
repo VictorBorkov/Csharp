0567518d16fbcb1f14ba9e5a4bf677d86799f5b3
Виктор Борков <mazerat_101@mail.ru>
2023-01-30 18:34:34 +0300

    Написал код

---
+++ Seminar1-HomeWork/3.DaysOfWeek/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// По заданному номеру дня недели вывести его название
+
+Console.Write("Напишите любой номер дня недели: ");
+int W = Convert.ToInt32(Console.ReadLine()!);
+
+if (W >= 1 && W <= 7)
+{
+    if (W == 1) Console.WriteLine("ПН");
+    if (W == 2) Console.WriteLine("ВТ");
+    if (W == 3) Console.WriteLine("СР");
+    if (W == 4) Console.WriteLine("ЧТ");
+    if (W == 5) Console.WriteLine("ПТ");
+    if (W == 6) Console.WriteLine("СБ");
+    if (W == 7) Console.WriteLine("ВС");
+}
+else Console.WriteLine("Такого дня недели не существует");
