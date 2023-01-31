a1773ce5b545c77bc31e8014cd939d12a66c846a
Виктор Борков <mazerat_101@mail.ru>
2023-01-31 19:10:40 +0300

    Сделал по методу

--- Practice/14.Method/Program.cs
+++ Practice/14.Method/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 // Найти наибольшее значение 9 значений "стихийным подходом"
-
+/*
 int a1 = 23;
 int b1 = 1;
 int c1 = 9;
@@ -26,3 +26,33 @@ if (c3 > max) max = c3;
 
 Console.Write("Наибольшее значение:");
 Console.Write(max);
+*/
+
+
+// Найти наибольшее значение 9 значений "методом" или "через функцию"
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 23;
+int b1 = 1;
+int c1 = 90000;
+
+int a2 = 231;
+int b2 = 233;
+int c2 = 43;
+
+int a3 = 8;
+int b3 = 874;
+int c3 = 1000;
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+
+Console.Write("Наибольшее значение: ");
+Console.WriteLine(max);
