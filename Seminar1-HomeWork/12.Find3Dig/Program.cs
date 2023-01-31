e80d5ecd625b7dec3e6547c22eb1ba17356ce25d
Виктор Борков <mazerat_101@mail.ru>
2023-01-31 13:55:48 +0300

    Написал код

---
+++ Seminar1-HomeWork/12.Find3Dig/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Найти третью цифру числа или сообщить, что её нет
+
+int N = new Random().Next(-10000, 10001);
+Console.Write("Сгенерировано число: ");
+Console.WriteLine(N);
+
+if (N < -99 || N > 99)
+{
+    while (N < -999 || N > 999)
+    {
+        N = N / 10;
+    }
+    N = N % 10;
+    if (N < 0) N = -1 * N;
+    Console.Write("Третья цифра: ");
+    Console.WriteLine(N);
+}
+else Console.Write("Третьей цифры нет в этом числе!");
