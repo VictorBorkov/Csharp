1225af721ae72fd27d44fb7bce98a494340b783b
Виктор Борков <mazerat_101@mail.ru>
2023-02-01 21:01:48 +0300

    Сделал два метода, заполнения массива и печати массива

---
+++ Practice/16.FillArray/Program.cs
@@ -0,0 +1,30 @@
+﻿// Заполнить одномерный массив случайными числами
+
+
+void F(int[] col)    // метод заполнения массива
+{
+    int i = 0;
+
+    while (i < col.Length)
+    {
+        col[i] = new Random().Next(1, 10);
+        i++;
+    }
+}
+
+void P(int[] col2)    // метод вывода его на экран
+{
+    int i2 = 0;
+
+    while (i2 < col2.Length)
+    {
+        Console.WriteLine(col2[i2]);
+        i2++;
+    }
+    
+}
+
+int[] array = new int[10];
+
+F(array);  
+P(array);
