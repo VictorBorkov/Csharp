a9ffeca4228e0e533e52d4309a2b98e485a72d2c
Виктор Борков <mazerat_101@mail.ru>
2023-02-02 00:16:26 +0300

    Подправил код

--- Practice/20.Palindrome/Program.cs
+++ Practice/20.Palindrome/Program.cs
@@ -7,8 +7,8 @@ int P = Convert.ToInt32(Console.ReadLine());
 int first = 0; int second = 0; int fourth = 0; int fifth = 0;
 
 fifth = P % 10;
-fourth = P / 10; fourth = fourth % 10;
-second = P / 1000; second = second % 10;
+fourth = P / 10 % 10;
+second = P / 1000 % 10;
 first = P / 10000;
 
 if (first == fifth && second == fourth) Console.WriteLine("Да, это палиндром");
