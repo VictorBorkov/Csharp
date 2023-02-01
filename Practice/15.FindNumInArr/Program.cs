9346ccf850101708d24f4f8b4ad8055c67531a18
Виктор Борков <mazerat_101@mail.ru>
2023-02-01 20:13:03 +0300

    Написал простой код

---
+++ Practice/15.FindNumInArr/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+// Найти в одномерном массиве число, которое задаёт пользователь
+
+int[] array = {2, 40, 5, 345, 23, -9, 23};
+int F = 23;
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] == F)
+    {
+        Console.Write("Индекс: ");
+        Console.WriteLine(i);
+        break;
+    }
+    i++;
+}
+*/
+
