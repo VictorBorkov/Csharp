6972a5e7fc497395ae90b43fc345b7420a70f8ec
Виктор Борков <mazerat_101@mail.ru>
2023-02-01 23:16:48 +0300

    Подправил еще раз

--- Practice/17.Multiple-7-23/Program.cs
+++ Practice/17.Multiple-7-23/Program.cs
@@ -3,11 +3,9 @@
 Console.Write("Задайте число: ");
 int N = int.Parse(Console.ReadLine());
 
-if (N % 7 == 0 && N % 23 == 0)
+if (N % 7 == 0 || N % 23 == 0)
 {
-    Console.WriteLine("Да, число " + N + " является кратным числам 7 и 23");
+    if (N % 7 == 0) Console.WriteLine("Да, число " + N + " является кратным числам 7");
+    if (N % 23 == 0) Console.WriteLine("Да, число " + N + " является кратным числам 23");
 }
-else
-{
-    Console.WriteLine("Нет, не кратное");
-}
+else Console.WriteLine("Нет, не кратное");
