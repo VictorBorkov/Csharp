1d576edaf89c79bf7bb27531c0ea07aece3fa0c2
Виктор Борков <mazerat_101@mail.ru>
2023-02-02 21:51:55 +0300

    Начало положено

---
+++ Seminar2-HomeWork/4.FindDisIn2D3D/Program.cs
@@ -0,0 +1,40 @@
+﻿
+// Найти расстояние между точками в пространстве 2D/3D
+
+Console.WriteLine("Выбирите систему в которой будем работать, напишите 2D или 3D: ");
+string D = Console.ReadLine()!;
+
+if (D.ToUpper() == "2D")
+{
+    Console.WriteLine("Вы выбрали двухмерное пространство!");
+    
+}
+else if (D.ToUpper() == "3D")
+{
+    Console.WriteLine("Вы выбрали треххмерное пространство!");
+}
+else
+{
+    Console.WriteLine("Вы опечатались, введите 2D или 3D");
+}
+
+
+
+
+
+
+/*
+Console.WriteLine("Чтобы найти расстояние между точками в 3D измерении - введите координаты x, y, z. Если нужно 2D, тогда для одной любой координаты введите 0 ");
+Console.Write("Введите координату x: ");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z: ");
+int Z = Convert.ToInt32(Console.ReadLine());
+
+if (X == 0 || Y == 0 || Z == 0)
+{
+    Console.WriteLine("Вы выбрали 2D измерение!");
+
+}
+*/
