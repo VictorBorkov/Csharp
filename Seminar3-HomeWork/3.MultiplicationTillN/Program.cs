9bfe616b130298e9f36c5a90b17e41fb6b8d253a
Виктор Борков <mazerat_101@mail.ru>
2023-02-03 00:35:51 +0300

    Написал код

---
+++ Seminar3-HomeWork/3.MultiplicationTillN/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Напишите число, до которого будем выполнять произведенеие: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N > 1)
+{
+    int count = 1;
+    int M = 1;
+    
+    while (count <= N)
+    {
+        M = M * count;
+        count++;
+    }
+    Console.Write($"Произведение всех чисел от 1 до {N} равно {M}");
+}
+else Console.WriteLine("Такое значение не допустимо!");
