17603a58d2b5a7b46b7f6882619b805f779edce5
Виктор Борков <mazerat_101@mail.ru>
2023-02-02 23:43:27 +0300

    Написал код

---
+++ Seminar3-HomeWork/1.CubeOfNum/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// Найти кубы чисел от 1 до N
+
+Console.Write("Введите любое значение, до которого будем считать кубы чисел: ");
+int end = Convert.ToInt32(Console.ReadLine());
+
+if (end > 1)
+{
+    Console.Write("Кубы чисел:");
+    int C = 0;
+    int p = 1;
+    
+    while (p <= end)
+    {
+        C = p * p * p;
+        Console.Write($" {C}");
+        p++;
+    }
+}
+else Console.WriteLine("Такое значение не допустимо!");
