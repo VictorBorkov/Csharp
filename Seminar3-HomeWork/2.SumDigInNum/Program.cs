fdbf20c9ee4b43c2a3c8551680fc290ad58f87c4
Виктор Борков <mazerat_101@mail.ru>
2023-02-03 00:14:13 +0300

    Написал код

---
+++ Seminar3-HomeWork/2.SumDigInNum/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Подсчитать сумму цифр в числе
+
+Console.Write("Введите любое целое число: ");
+int Num = Convert.ToInt32(Console.ReadLine()!);
+int Sum = 0;
+int Step = 0;
+
+while (Num != 0)
+{
+    Step = Num % 10;
+    Num = Num / 10;
+    Sum = Sum + Step;
+
+}
+
+if (Sum > 0) Console.Write($"Сумма цифр: {Sum}");
+if (Sum < 0) Console.Write($"Сумма цифр: {-Sum}");
+
