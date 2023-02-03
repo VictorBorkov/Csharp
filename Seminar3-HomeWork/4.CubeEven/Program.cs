12c2aaac017a5693e3254d81475e241bc54b7070
Виктор Борков <mazerat_101@mail.ru>
2023-02-03 22:23:19 +0300

    Написал код

---
+++ Seminar3-HomeWork/4.CubeEven/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Write("Задайте любое целое число: ");
+int C = Convert.ToInt32(Console.ReadLine());
+int LD = 0;
+
+LD = C % 10;
+
+if (C == 0) Console.Write("Ноль не пойдёт ;D");
+else if (LD % 2 == 0) Console.Write("Куб вашего числа равен: " + C * C * C);
+else Console.Write("Ваше число заканчивается на нечётную цифру.");
+
