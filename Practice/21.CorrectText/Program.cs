406d217879918bbacb94415f0f991860fa9f4cd6
Виктор Борков <mazerat_101@mail.ru>
2023-02-04 19:23:15 +0300

    Написал код

---
+++ Practice/21.CorrectText/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// Дан текст, все пробелы нужно заменить на черточки, "к" на "К", "с" на "С"
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
++ "ежели бы вас послали вместо нашего милого Винценгероде,"
++ "вы бы взяли приступом согласие прусского короля. "
++ "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+
+    return result;
+}
+
+Console.WriteLine(text);
+Console.WriteLine();  // это просто разрыв
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();  // это просто разрыв
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();  // это просто разрыв
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
