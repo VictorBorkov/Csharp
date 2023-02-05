df2aad8b8be1d1a66cd9da4b913d55257a3c1add
Виктор Борков <mazerat_101@mail.ru>
2023-02-05 22:43:31 +0300

    Добавил обратный сорт

--- Practice/22.PutInOrderArr/Program.cs
+++ Practice/22.PutInOrderArr/Program.cs
@@ -1,6 +1,45 @@
 ﻿
 // Упорядочить массив. Алгоритм "сортировка методом выбора" или "сортировка методом min/max"
 
+// int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} | ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+        
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//         }
+        
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
+
+
+// PrintArray(arr);
+// SelectionSort(arr);
+// PrintArray(arr);
+
+
+
+
+
+// Упорядочить массив. Теперь от большего к меньшему. Алгоритм "сортировка методом выбора" или "сортировка методом min/max"
+
 int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
 
 void PrintArray(int[] array)
@@ -16,19 +55,18 @@ void SelectionSort(int[] array)
 {
     for (int i = 0; i < array.Length - 1; i++)
     {
-        int minPosition = i;
+        int maxPosition = i;
         
         for (int j = i + 1; j < array.Length; j++)
         {
-            if (array[j] < array[minPosition]) minPosition = j;
+            if (array[j] > array[maxPosition]) maxPosition = j;
         }
         
         int temporary = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temporary;
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
     }
 }
-
 
 PrintArray(arr);
 SelectionSort(arr);
