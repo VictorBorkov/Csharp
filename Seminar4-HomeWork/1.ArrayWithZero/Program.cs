a31c1c1753fbd3413f68447d3339c73c69e6fc53
Виктор Борков <mazerat_101@mail.ru>
2023-02-05 23:03:19 +0300

    Написал код

---
+++ Seminar4-HomeWork/1.ArrayWithZero/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Задать массив из 8 элементов, заполненных нулями и единицами, вывести их на экран
+
+int[] Arr = {0, 1, 0, 0, 1, 0, 1, 1};
+
+void Show(int[] AnyArr)
+{
+    for (int i = 0; i < AnyArr.Length; i++)
+    {
+        Console.Write($" {AnyArr[i]} ");
+    }
+    
+}
+
+Console.Write("Вот он этот массив: ");
+Show(Arr);
