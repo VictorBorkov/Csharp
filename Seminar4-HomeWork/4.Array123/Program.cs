a8e5f6a4d416ec4176dee3d1d3e917283483cd78
Виктор Борков <mazerat_101@mail.ru>
2023-02-06 20:33:11 +0300

    Написал код

---
+++ Seminar4-HomeWork/4.Array123/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10, 99]
+
+void Fill(int[] AnyArr)
+{
+    for (int i = 0; i < AnyArr.Length; i++)
+    {
+        AnyArr[i] = new Random().Next(-500, 500);
+    }
+}
+
+void Show(int[] AnyArr)
+{
+    Console.WriteLine("Массив данных:");
+    for (int i = 0; i < AnyArr.Length; i++)
+    {
+        Console.Write($"{AnyArr[i]}|");
+    }
+    Console.WriteLine();
+}
+
+void Scan(int[] AnyArr)
+{
+    int count = 0;
+    for (int i = 0; i < AnyArr.Length; i++)
+    {
+        if (AnyArr[i] > 9 && AnyArr[i] < 100) count++;
+    }
+    Console.WriteLine($"Количество элементов получилось: {count}.");
+}
+
+int[] Arr = new int[123];
+Fill(Arr);
+Show(Arr);
+Scan(Arr);
