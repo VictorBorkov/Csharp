e28c177b54e769c7ea7c94395e2ddddfafa03eea
Виктор Борков <mazerat_101@mail.ru>
2023-02-07 21:54:05 +0300

    Написал код

---
+++ Seminar4-HomeWork/6.MultiplyOpposite/Program.cs
@@ -0,0 +1,38 @@
+﻿
+// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпослдений и т.д.
+
+void Fill(int[] AnyArr)
+{
+    for (int i = 0; i < AnyArr.Length; i++)
+    {
+        AnyArr[i] = new Random().Next(0, 20);
+    }
+}
+
+void Show(int[] AnyArr)
+{
+    Console.Write("Массив данных: ");
+    for (int i = 0; i < AnyArr.Length; i++)
+    {
+        Console.Write($"{AnyArr[i]} | ");
+    }
+    Console.WriteLine();
+}
+
+void MulPair(int[] AnyArr)
+{
+    int Mul = 0;
+    int j = AnyArr.Length - 1;
+    Console.Write("Произведения пар: ");
+    for (int i = 0; i < (AnyArr.Length/2); i++)
+    {
+        Mul = AnyArr[i] * AnyArr[j];
+        Console.Write($"{Mul} ");
+        j--;
+    }
+}
+
+int[] Arr = new int[9];
+Fill(Arr);
+Show(Arr);
+MulPair(Arr);
